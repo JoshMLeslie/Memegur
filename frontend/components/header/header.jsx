512b89cdf581ade020db32c5f5a1033f0c010807
JoshMLeslie <hello@joshmleslie.com>
2018-05-09 10:14:09 -0400

    morning revisions. Still working on session form placement

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -5,25 +5,43 @@ import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 import { AuthRoute } from '../../util/route_util';
 import FaCloudUpload from 'react-icons/lib/fa/cloud-upload';
 
-
-
 class Header extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showForm: false,
+      formType: "",
+    };
+
+    this.showForm = this.showForm.bind(this);
+    // showForm = (e) => (); if I want to remove the binding later.
   }
 
+  showForm (e) {
+    // sets form type by checking for 'in' from 'Login' and sets state for the relative form.
+    const formType = (
+      (/in/).test(e.target.innerText) ? "showLogin" : "showSignup"
+    );
+
+    this.setState({formType: [formSelect]});
+    this.setState({showForm: !this.state.showForm});
+      // then conditionally render form based on state here
+  }
   render () {
     const currentUser = this.props.currentUser;
     const logout = this.props.logout;
 
     const MemegurLogo = () => (
+      // always renders site block (on left)
       <div id="header-navigation">
         <Link to="/" className="button-link" > MEMEGUR </Link>
         <FaCloudUpload size={30} style={{color: '#00BFFF'}}/>
       </div>
     );
-
+    // renders on right side
     const UserBlock = () => (
+      // renders when a user is logged in
       <div id="header" className="user-block">
         <h2>Hello, {currentUser.username}</h2>
         <button onClick={logout}>Log Out</button>
@@ -31,16 +49,10 @@ class Header extends React.Component {
     );
 
     const LoginLinks = () => (
+      // renders when no one is logged in && not on a form page
       <div id="header-session-links" className="session-links">
-        <Link to='/login'>Login! </Link>
-        <Link to='/signup'>Sign Up! </Link>
-      </div>
-    );
-
-    const LoginForms = () => (
-      <div id="session-form">
-        <AuthRoute path="/login" component={ LoginFormContainer } />
-        <AuthRoute path="/signup" component={ SignupFormContainer } />
+        <button onClick={this.showForm}>Login! </button>
+        <button onClick={this.showForm}>Sign Up! </button>
       </div>
     );
 
@@ -48,7 +60,8 @@ class Header extends React.Component {
       if (currentUser) {
         return ( <UserBlock /> );
       } else if ( !(/\/\w+/).test(this.props.history.location.pathname) ) {
-        return ( <LoginLinks /> ); // i.e. the long way to say: only on "/"
+        // the long way to say: only on "/"
+        return ( <LoginLinks /> );
       } else {
         return ( <LoginForms /> );
       }
